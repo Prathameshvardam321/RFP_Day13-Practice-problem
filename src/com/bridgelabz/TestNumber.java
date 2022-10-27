8c40ca54b8d3d3a153e1ee9847464c033e764ecf
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-27 22:22:35 +0530

    Added MaximumNumberUC3

--- src/com/bridgelabz/TestNumber.java
+++ src/com/bridgelabz/TestNumber.java
@@ -1,18 +1,18 @@
 package com.bridgelabz;
 
 public class TestNumber<T> {
-   static Float toFindMaximum(Float[] intArray){
-       Float max =0.0f;
-       for (int i=0;i<intArray.length-1;i++){
-          if (intArray[i].compareTo(intArray[i+1])==1&&intArray[i]>max){
-              max=intArray[i];
-          }
-       }
+   static String toFindMaximum(String[] intArray){
+       String max =intArray[0];
+       for (int i=0;i<intArray.length;i++){
+           if (intArray[i].compareTo(max) == 1) {
+                   max = intArray[i];
+               }
+           }
        return max;
     }
     public static void main(String[] args) {
-        Float[] intArray = {121.1f,13.21f,45.12f};
-       float result = toFindMaximum(intArray);
+        String[] stringArray = {"Potato","Banana","Carrot"};
+       String result = toFindMaximum(stringArray);
         System.out.println(result);
     }
 }
